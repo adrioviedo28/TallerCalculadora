184d89cc1292fbf071daa8572ddbf391e9cf74fb
adrioviedo28 <adrioviedov@gmail.com>
2024-09-06 11:35:38 -0500

    Taller de calculadora ingreso de notas importantes

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -2,12 +2,13 @@ import java.util.Scanner;
 
 public class Calculadora {
 
-    // Constructor de la clase Calculadora
+    // el public Calculadora es el Constructor de la clase Calculadora
     public Calculadora() {
-        // El constructor puede estar vacío si no es necesario inicializar nada
     }
 
-    // Método para sumar dos números
+    // siguiente es el Método para sumar dos números.
+    // "Double" :es un tipo de dato primitivo que se usa para representar números en coma flotante,
+    // es decir, números que pueden tener una parte decimal
     public double sumar(double a, double b) {
         return a + b;
     }
@@ -24,21 +25,28 @@ public class Calculadora {
 
     // Método para dividir dos números
     public double dividir(double a, double b) {
-        // Verificar si el divisor es 0 para evitar la división por cero
+        // Verificar si el divisor es 0 para evitar la división por cero,
+        //se usa para manejar de manera controlada la situación en la que se intenta
+       // realizar una división por cero. La división por cero es una operación matemática indefinida
+        // y puede causar problemas en la ejecución del programa
         if (b == 0) {
             throw new ArithmeticException("División por cero no permitida.");
         }
         return a / b;
     }
 
-    // Método principal para probar la calculadora
+    // Usar la Clase en el Método Principal para probar la calculadora.
+    // método main para probar la clase Calculadora.
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         // Crear una instancia de la clase Calculadora
+        //Calculadora calc = new Calculadora(); crea un nuevo objeto de la clase Calculadora
         Calculadora calcu = new Calculadora();
 
         // Leer los dos números del usuario
+        //se usa scaner para  leer la entrada del usuario y Se solicita al usuario que ingrese
+        // dos números (num1 y num2).
         System.out.print("Ingrese el primer número: ");
         double num1 = scanner.nextDouble();
         System.out.print("Ingrese el segundo número: ");
@@ -54,6 +62,11 @@ public class Calculadora {
         int opcion = scanner.nextInt();
 
         // Ejecutar la operación seleccionada
+        //Se utiliza un switch para manejar las diferentes operaciones
+        // y un try-catch para capturar cualquier excepción, como la división por cero
+        //try es una palabra clave utilizada en el manejo de excepciones. Permite manejar errores de
+        // forma controlada y prevenir que un programa se detenga abruptamente si ocurre una
+        // excepción durante su ejecución
         try {
             double resultado;
             switch (opcion) {
